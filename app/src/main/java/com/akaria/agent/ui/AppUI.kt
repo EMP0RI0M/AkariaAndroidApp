531ad97fba74e3611ca04ead118cfab093ee7020
root <root@localhost.localdomain>
2026-08-01 19:27:12 -1000

    UI: Add missing clickable import in AppUI.kt

--- app/src/main/java/com/akaria/agent/ui/AppUI.kt
+++ app/src/main/java/com/akaria/agent/ui/AppUI.kt
@@ -8,6 +8,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
